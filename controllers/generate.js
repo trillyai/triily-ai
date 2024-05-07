2e770b61bab7a58bb3f92d3d1aa8e083e02def38
Burak TAŞÇI <buraktasci2001@gmail.com>
2024-05-07 03:04:37 +0300

    base query for overpass api

--- controllers/generate.js
+++ controllers/generate.js
@@ -1,5 +1,19 @@
-export function generateTrip( req, res ) {
+export async function generateTrip(req, res) {
+  let result = await fetch("https://overpass-api.de/api/interpreter", {
+    method: "POST",
+    // The body contains the query
+    // to understand the query language see "The Programmatic Query Language" on
+    // https://wiki.openstreetmap.org/wiki/Overpass_API#The_Programmatic_Query_Language_(OverpassQL)
+    body:
+      "data=" +
+      encodeURIComponent(`
+      [out:json];
+      area[name="İstanbul"];
+      nwr[tourism = "museum"](area);
+      out center;
+        `),
+  }).then((res) => res.json());
   res.status(200).send({
-    data: "Hello Trip!",
+    data: result,
   });
 }
