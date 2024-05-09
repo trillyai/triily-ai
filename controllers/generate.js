6ccecc242685b766dd7be0599ff8c0d58f2133c1
Burak TAŞÇI <buraktasci2001@gmail.com>
2024-05-09 15:58:33 +0300

    parameters search

--- controllers/generate.js
+++ controllers/generate.js
@@ -1,16 +1,30 @@
+// taginfo: https://taginfo.openstreetmap.org/tags
+
 export async function generateTrip(req, res) {
+  const {
+    views,
+    historical,
+    cultural,
+    outdoor,
+    cuisine,
+    religious,
+    shopping,
+    entertainment,
+  } = req.query;
   const timeout = 25;
   let result = await fetch("https://overpass-api.de/api/interpreter", {
     method: "POST",
-    // The body contains the query
-    // to understand the query language see "The Programmatic Query Language" on
-    // https://wiki.openstreetmap.org/wiki/Overpass_API#The_Programmatic_Query_Language_(OverpassQL)
     body:
       "data=" +
       encodeURIComponent(`
       [out:json][timeout:${timeout}];
-      area[name="İstanbul"];
-      nwr[tourism][tourism!="hostel"][tourism!="hotel"][tourism!="guest_house"][tourism!="apartment"](area)[tourism!="information"](area)[name](area);
+      area[name="İstanbul"]->.a;
+      (
+        nwr[tourism][name][wikidata][tourism!="hostel"][tourism!="hotel"][tourism!="viewpoint"][tourism!="guest_house"][tourism!="apartment"](area.a)[tourism!="information"](area.a)[tourism!="motel"][tourism!="caravan_site"];
+        nwr[religion][wikipedia](area.a);
+        nwr[tourism="viewpoint"](area.a);
+        nwr[~"^historic(:.*)?$"~"."][~"^wiki.*?$"~"."](area.a);
+      );
       out center;
       `),
   }).then((res) => res.json());
---
+++ docs/parameters.md
@@ -0,0 +1,31 @@
+1. **Scenic Views and Photography:**
+   - Parameter: `views`
+   - Tag: `tourism=viewpoint`
+
+2. **Historical Sites and Monuments:**
+   - Parameter: `historical`
+   - Tag: `historic=*`
+
+3. **Cultural Attractions (Museums and Art Galleries):**
+   - Parameter: `cultural`
+   - Tags: `tourism=museum`, `tourism=art_gallery`
+
+4. **Outdoor Activities (Hiking, Cycling, Camping):**
+   - Parameter: `outdoor`
+   - Tags: `leisure=*`, `sport=*`, `tourism=attraction`
+
+5. **Dining Out and Local Cuisine:**
+   - Parameter: `cuisine`
+   - Tags: `amenity=restaurant`, `cuisine=*`
+
+6. **Religious Sites and Spiritual Experiences:**
+   - Parameter: `religious`
+   - Tag: `amenity=place_of_worship`
+
+7. **Shopping and Markets:**
+   - Parameter: `shopping`
+   - Tags: `shop=*`, `tourism=marketplace`
+
+8. **Entertainment Venues (Theaters, Cinemas, Music Venues):**
+   - Parameter: `entertainment`
+   - Tags: `amenity=theatre`, `amenity=cinema`, `amenity=music_venue`
